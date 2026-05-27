ec5f12d94bbe26a8e930e078f2a4ccbf3ea230cb
creatijourny <misbahulhaque.gmail.com>
2026-05-27 08:43:25 +0600

    api for delete function created and database connection

--- index.js
+++ index.js
@@ -61,6 +61,17 @@ async function run() {
       res.json(result)
     })
 
+    app.delete('/room/:id', async (req, res) => {
+      const { id } = req.params
+      // const updatedData = req.body
+
+      const result = await roomCollection.deleteOne(
+        {_id: new ObjectId(id)}
+        // {$set: updatedData}
+      )
+      res.json(result)
+    })
+
 
     
     await client.db("admin").command({ ping: 1 });
